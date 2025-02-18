854571ff39b4415c096d3d17f185d70fe8679251
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-02-18 10:07:03 +0200

    Install Tanstack Query and devtools, making some fake requests

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,24 @@
+import { getCategories, getProducts } from "@/utils/api";
+import { useQuery } from "@tanstack/react-query";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const { data: products } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
+
+  console.log(products);
+
+  const { data: categories = [] } = useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategories,
+  });
+
+  const allCategories = ["All", ...categories];
+
+  console.log(categories);
+
   return (
     <View
       style={{
@@ -8,8 +26,6 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    ></View>
   );
 }
